168eb233907c27bc36ca4fc1eecff745275f978a
Daniel Idoko <danielidoko46@gmail.com>
2026-03-21 13:25:17 +0100

    fix server.ts

--- server.ts
+++ server.ts
@@ -17,33 +17,6 @@ import { dashboardRoutes } from "./src/routes/dashboard.routes";
 
 const app: Application = express();
 
-// CORS
-const allowedOrigins = [
-  "http://localhost:5173",
-  "https://managel-app.vercel.app",
-  "http://managel-app.vercel.app",
-];
-
-app.use(
-  app.use(
-    cors({
-      origin: (origin, callback) => {
-        if (!origin) return callback(null, true);
-        const isAllowed = allowedOrigins.some((o) => o === origin);
-        if (isAllowed) {
-          callback(null, true);
-        } else {
-          callback(new Error(`CORS: ${origin} not allowed`));
-        }
-      },
-      credentials: true,
-      optionsSuccessStatus: 200,
-    }),
-  ),
-);
-
-app.options("*", cors());
-
 /**
  * DATABASE CONNECTION (Serverless Optimized)
  */
@@ -73,7 +46,30 @@ const connectDBMiddleware = async (
 /**
  * MIDDLEWARES
  */
+// CORS
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://managel-app.vercel.app",
+  "http://managel-app.vercel.app",
+];
 
+app.use(
+  app.use(
+    cors({
+      origin: (origin, callback) => {
+        if (!origin) return callback(null, true);
+        const isAllowed = allowedOrigins.some((o) => o === origin);
+        if (isAllowed) {
+          callback(null, true);
+        } else {
+          callback(new Error(`CORS: ${origin} not allowed`));
+        }
+      },
+      credentials: true,
+      optionsSuccessStatus: 200,
+    }),
+  ),
+);
 // 2. Parsers & Logging
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));

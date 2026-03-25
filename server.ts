99f8d3a6dc281268a7b7b20076ef7e185bbcee95
Daniel Idoko <danielidoko46@gmail.com>
2026-03-25 16:00:48 +0100

    fix cors issue again

--- server.ts
+++ server.ts
@@ -78,12 +78,14 @@ app.use(
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
 // app.use(
 //   helmet({
 //     crossOriginResourcePolicy: { policy: "cross-origin" },
 //   }),
 // );
 // Logging
+
 app.use(morgan("dev"));
 
 app.use(globalLimiter);
--- src/middlewares/rateLimiter.ts
+++ src/middlewares/rateLimiter.ts
@@ -14,6 +14,7 @@ export const globalLimiter = rateLimit({
     // Skip rate limiting in development
     return config.NODE_ENV === "development";
   },
+  validate: { xForwardedForHeader: false },
 });
 
 /**

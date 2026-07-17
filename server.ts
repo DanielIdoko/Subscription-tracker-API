77ee13387dde5964d185e25683cdd3031d1e8b0f
Daniel Idoko <danielidoko46@gmail.com>
2026-07-17 11:05:48 +0100

    fix app for render deployment

--- server.ts
+++ server.ts
@@ -1,14 +1,12 @@
 import dns from "dns";
 dns.setServers(["1.1.1.1", "8.8.8.8"]);
 
-import express, { Application, Request, Response, NextFunction } from "express";
+import express, { Application, Request, Response } from "express";
 import morgan from "morgan";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 
-// import { config } from "./src/config/env";
 import { connectDatabase } from "./src/database/connection.js";
-import { applySecurityMiddlewares } from "./src/middlewares/security.js";
 import { globalLimiter } from "./src/middlewares/rateLimiter.js";
 import { errorHandler, notFoundHandler } from "./src/middlewares/index.js";
 
@@ -17,10 +15,9 @@ import { authRoutes } from "./src/routes/auth.routes.js";
 import { userRoutes } from "./src/routes/user.routes.js";
 import { subscriptionRoutes } from "./src/routes/subscription.routes.js";
 import { dashboardRoutes } from "./src/routes/dashboard.routes.js";
-import helmet from "helmet";
 
+const PORT = process.env.PORT || 5500;
 const app: Application = express();
-
 
 /**
  * MIDDLEWARES
@@ -33,9 +30,8 @@ const allowedOrigins = [
 
 app.use(
   cors({
-    origin: (origin = process.env.CORS_ORIGIN, callback) => {
+    origin: (origin, callback) => {
       if (!origin) return callback(null, true);
-
       if (allowedOrigins.includes(origin)) {
         callback(null, true);
       } else {
@@ -48,22 +44,15 @@ app.use(
   }),
 );
 
-// 2. Parsers & Logging
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-
-// Logging
-
 app.use(morgan("dev"));
-
 app.use(globalLimiter);
 
 /**
  * ROUTES
  */
-let isConnected = false;
-
 app.get("/health", (_req: Request, res: Response) => {
   res.status(200).json({
     success: true,
@@ -72,7 +61,7 @@ app.get("/health", (_req: Request, res: Response) => {
   });
 });
 
-app.get("/api/v1/", (_req: Request, res: Response) => {
+app.get("/api/v1", (_req: Request, res: Response) => {
   res.status(200).json({
     success: true,
     message: "API test ran successfully",
@@ -80,39 +69,30 @@ app.get("/api/v1/", (_req: Request, res: Response) => {
   });
 });
 
-app.get("/", async (_req: Request, res: Response) => {
-  await connectDatabase();
-  isConnected = true;
+app.get("/", (_req: Request, res: Response) => {
   res.status(200).json({
     success: true,
-    message: "Managel API running successfully ",
+    message: "Managel API running successfully",
     version: "1.0.0",
-    dbConnected: isConnected,
+    dbConnected: true,
   });
 });
 
-// Apply DB connection middleware to all API routes
-// apiV1.use(connectDBMiddleware);
-
-/**
- * API ROUTES
- */
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/users", userRoutes);
 app.use("/api/v1/subscriptions", subscriptionRoutes);
 app.use("/api/v1/dashboard", dashboardRoutes);
 
-/**
- * ERROR HANDLING
- */
 app.use(notFoundHandler);
 app.use(errorHandler);
 
-export default app;
-
-// if (process.env.NODE_ENV === "development") {
-app.listen(Number(process.env.PORT), '0.0.0.0', async () => {
-  await connectDatabase();
-  console.log(`Server running on http://localhost:${process.env.PORT}`);
+app.listen(Number(PORT), "0.0.0.0", async () => {
+  try {
+    await connectDatabase();
+    console.log(`Server running securely on port ${PORT}`);
+  } catch (error) {
+    console.error("Database failed to connect on startup:", error);
+  }
 });
-// }
+
+export default app;
